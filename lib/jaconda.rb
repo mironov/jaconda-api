3147a7967eff7fbe3cffe1ceb20cb192dca65fdb
Anton Mironov <ant.mironov@gmail.com>
2010-03-24 12:16:30 +0500

    add initial jaconda api support

---
+++ lib/jaconda.rb
@@ -0,0 +1,31 @@
+require 'active_resource'
+
+module Jaconda
+  class API < ActiveResource::Base
+    self.site = "http://localhost:3000"
+    self.prefix = "/api/"
+    
+    def self.authenticate(api_token)
+      self.password = 'x'
+      self.user = api_token
+    end
+  end
+
+  class Room < API
+    def users(options = {})
+      User.find(:all, :params => options.update(:room_id => id))
+    end
+
+    def messages(options = {})
+      Message.find(:all, :params => options.update(:room_id => id))
+    end
+  end
+
+  class User < API
+    self.prefix = "/api/rooms/:room_id/"
+  end
+
+  class Message < API
+    self.prefix = "/api/rooms/:room_id/"
+  end
+end
